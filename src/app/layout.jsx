46b689b210e90b131528d7faaab04ca3b7c54ec2
Cinquin Andy <andy.cinquin@infotel.com>
2024-04-28 18:37:48 +0200

    🚀 Update ESLint configuration and package dependencies
    - Updated ESLint config to extend additional rules
    - Added new dev dependencies for eslint-config-prettier, husky, lint-staged
    - Updated versions of various packages including postcss, ansi-escapes

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,32 +1,35 @@
 import glob from 'fast-glob'
 import {Layout} from '@/components/Layout'
+import {Kanit, Playpen_Sans} from 'next/font/google'
 
 import '@/styles/tailwind.css'
 
 export const metadata = {
     title: {
-        template: '%s - ForVoyez API Reference',
-        default: 'ForVoyez API Reference',
+        template: '%s - ForVoyez API Reference', default: 'ForVoyez API Reference',
     },
 }
 
+// fixme replace that with your chosen font
+const kanit = Kanit({
+    weight: ['100', '300', '400', '700', '900'], subsets: ['latin'], variable: '--font-kanit', style: ['normal', 'italic'],
+})
+
+// fixme replace that with your chosen font
+const playpen_sans = Playpen_Sans({
+    weight: ['200', '300', '400', '500', '600', '700', '800'], subsets: ['latin'], variable: '--font-playpen_sans', style: ['normal'],
+})
+
 export default async function RootLayout({children}) {
     let pages = await glob('**/*.mdx', {cwd: 'src/app'})
-    let allSectionsEntries = await Promise.all(
-        pages.map(async (filename) => [
-            '/' + filename.replace(/(^|\/)page\.mdx$/, ''),
-            (await import(`./${filename}`)).sections,
-        ]),
-    )
+    let allSectionsEntries = await Promise.all(pages.map(async (filename) => ['/' + filename.replace(/(^|\/)page\.mdx$/, ''), (await import(`./${filename}`)).sections,]),)
     let allSections = Object.fromEntries(allSectionsEntries)
 
-    return (
-        <html lang="en" className="h-full" suppressHydrationWarning>
-        <body className="flex min-h-full bg-white antialiased">
-        <div className="w-full">
-            <Layout allSections={allSections}>{children}</Layout>
-        </div>
-        </body>
-        </html>
-    )
+    return (<html lang="en" suppressHydrationWarning className={`h-full ${kanit.variable} ${playpen_sans.variable}`}>
+    <body className="flex min-h-full bg-white antialiased">
+    <div className="w-full">
+        <Layout allSections={allSections}>{children}</Layout>
+    </div>
+    </body>
+    </html>)
 }
