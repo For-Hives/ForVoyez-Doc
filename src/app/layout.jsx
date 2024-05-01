5b8acb7585e6af67a7483f7e6a4904b1e852e3af
Cinquin Andy <andy.cinquin@infotel.com>
2024-05-01 13:49:56 +0200

    😎 change typo

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,8 +1,9 @@
 import glob from 'fast-glob'
 import {Layout} from '@/components/Layout'
-import {Kanit, Playpen_Sans} from 'next/font/google'
 
 import '@/styles/tailwind.css'
+import {Jost, Source_Sans_3} from 'next/font/google'
+
 
 export const metadata = {
     title: {
@@ -10,14 +11,18 @@ export const metadata = {
     },
 }
 
-// fixme replace that with your chosen font
-const kanit = Kanit({
-    weight: ['100', '300', '400', '700', '900'], subsets: ['latin'], variable: '--font-kanit', style: ['normal', 'italic'],
+const sourcesans = Source_Sans_3({
+    weight: ['300', '400', '700', '900'],
+    subsets: ['latin'],
+    variable: '--font-sourcesans',
+    style: ['normal', 'italic'],
 })
 
-// fixme replace that with your chosen font
-const playpen_sans = Playpen_Sans({
-    weight: ['200', '300', '400', '500', '600', '700', '800'], subsets: ['latin'], variable: '--font-playpen_sans', style: ['normal'],
+const jost = Jost({
+    weight: ['200', '300', '400', '500', '600', '700', '800'],
+    subsets: ['latin'],
+    variable: '--font-jost',
+    style: ['normal'],
 })
 
 export default async function RootLayout({children}) {
@@ -25,7 +30,7 @@ export default async function RootLayout({children}) {
     let allSectionsEntries = await Promise.all(pages.map(async (filename) => ['/' + filename.replace(/(^|\/)page\.mdx$/, ''), (await import(`./${filename}`)).sections,]),)
     let allSections = Object.fromEntries(allSectionsEntries)
 
-    return (<html lang="en" suppressHydrationWarning className={`h-full ${kanit.variable} ${playpen_sans.variable}`}>
+    return (<html lang="en" suppressHydrationWarning className={`h-full ${sourcesans.variable} ${jost.variable}`}>
     <body className="flex min-h-full bg-white antialiased">
     <div className="w-full">
         <Layout allSections={allSections}>{children}</Layout>
